4d0fd06ad3b4584028b9ee79910cc75c62ddf341
virenshah-cmd <virendra.shah@gds.ey.com>
2026-02-09 06:38:05 +0000

    Step 4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-f9w#!f7eqi&o81!fjq3@!_lk8q%z4xwc%0f2s9cm9v_9u@+r@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+codespace_name = os.environ.get('CODESPACE_NAME', None)
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_name:
+    ALLOWED_HOSTS.append(f'{codespace_name}-8000.app.github.dev')
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -14,6 +14,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
+import os
+
+def api_root(request):
+    codespace_name = os.environ.get('CODESPACE_NAME', 'localhost')
+    base_url = f"https://{codespace_name}-8000.app.github.dev/api/"
+    # For localhost, also allow http://localhost:8000/api/
+    localhost_url = "http://localhost:8000/api/"
+    return JsonResponse({
+        "activities": f"{base_url}activities/",
+        "users": f"{base_url}users/",
+        "teams": f"{base_url}teams/",
+        "leaderboard": f"{base_url}leaderboard/",
+        "workouts": f"{base_url}workouts/",
+        "activities_local": f"{localhost_url}activities/",
+        "users_local": f"{localhost_url}users/",
+        "teams_local": f"{localhost_url}teams/",
+        "leaderboard_local": f"{localhost_url}leaderboard/",
+        "workouts_local": f"{localhost_url}workouts/"
+    })
+
+urlpatterns = [
+    path('api/', api_root),
+    path('api/activities/', include('octofit_tracker.activities.urls')),
+    path('api/users/', include('octofit_tracker.users.urls')),
+    path('api/teams/', include('octofit_tracker.teams.urls')),
+    path('api/leaderboard/', include('octofit_tracker.leaderboard.urls')),
+    path('api/workouts/', include('octofit_tracker.workouts.urls')),
+    path('admin/', admin.site.urls),
+]
+"""octofit_tracker URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
